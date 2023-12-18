50db8353b4eb32f1c5eaab8bdc898dd188098afd
botirnabiev23 <botnab8@gmail.com>
2023-12-19 01:43:17 +0500

    Initial commit

---
+++ lib/player.dart
@@ -0,0 +1,15 @@
+class Player {
+  final String name;
+  final int number;
+  final String position;
+  final String photo;
+  final String value;
+
+  const Player({
+    required this.name,
+    required this.number,
+    required this.position,
+    required this.photo,
+    required this.value,
+  });
+}
